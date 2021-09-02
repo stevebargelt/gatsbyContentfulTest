56efbe839fcdbc5d0787e8f1c863dd06f88d9e00
Steve Bargelt <steve@bargelt.com>
2021-09-02 09:14:12 -0700

    Initial

---
+++ src/components/Layout/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { HeaderInner, HeaderWrapper } from './style';
+import Menu from './Menu';
+
+const Header = () => {
+    return (
+        <HeaderWrapper>
+            <HeaderInner>
+                <Menu />
+
+            </HeaderInner>
+        </HeaderWrapper>
+    )
+}
+
+export default Header;
